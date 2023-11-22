e52aa1dab1259dee4957c7383d7fa830b747e384
Master <nghiemxthai@gmail.com>
2023-11-21 20:58:35 -0800

    initial files added

---
+++ PSEUDO.md
@@ -0,0 +1,72 @@
+# 18 NoSQL: Social Network API
+1. Set up config - (make sure MONGOOSE connection are good).
+2. Set up route directory with CONTROLLERS and ROUTES in it.
+3. Set up Model directory 
+4. Set up Utils directory
+5. Set up NPM - express , mongoose, nodemon --watch
+6. Create Server.js
+
+
+Model User
+- username
+- email
+- thoughts ( ref 'thought')
+- friends ( ref 'User')
+- virtual friendCount - going to have a function to calculate length of the FRIEND array.
+
+
+Model Thought
+- thoughtText
+- createdAt
+- username ( ref 'userId' )
+- reaction ( array of reactionId references ) 
+(doesn't have to be a model, has to be a schema)
+- virtual reactionCount - going to have a function 
+to get the length of REACTIONS array
+
+
+Schema Reaction 
+- Id
+- body
+- username
+- createdAt
+This will not be a model, but rather will be used as the `reaction` field's subdocument schema in the `Thought` model.
+
+
+
+API Routes
+
+User route
+- get 1 findOne({ })
+- get all find({ })
+- put findOneAndUpdate({ })
+- create create(req.body)
+- delete findOneAndDelete({ })
+- POST findOneAndUpdate {$AddtoSet { }}
+- DELETE for friend request findOneAndUpdate {$pull} {new True}
+
+
+ 
+ \
+
+
+Thought route
+- get 1
+- get all
+- put 
+- create 
+- delete
+- ADD / DELETE Reaction
+
+
+
+
+
+CONTROLLER 
+Interact with the model
+modules.export = findAll(user)
+
+
+
+ROUTES interact with the Database
+render user
---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const cwd = process.cwd();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
+const activity = cwd.includes('01-Activities')
+  ? cwd.split('01-Activities')[1]
+  : cwd;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
+
+
+
+
+
+const { Schema, model } = require('mongoose');
+const Tag = require('./Tag');
+
+// Schema to create Post model
+const applicationSchema = new Schema(
+  {
+    published: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    buildSuccess: {
+      type: Boolean,
+      default: true,
+    },
+    description: {
+      type: String,
+      minLength: 15,
+      maxLength: 500,
+    },
+    tags: [Tag],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `getTags` that gets the amount of tags associated with an application
+applicationSchema
+  .virtual('getTags')
+  // Getter
+  .get(function () {
+    return this.tags.length;
+  });
+
+// Initialize our Application model
+const Application = model('application', applicationSchema);
+
+module.exports = Application;
+
