b70347989adfd0c6ce92a5aae5870476b0f275a2
Master <nghiemxthai@gmail.com>
2023-11-24 11:47:19 -0800

    create user routes added

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -21,3 +21,17 @@ router.get('/:id', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+
+// create new user
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+        res.status(200).json(userData);
+    } catch(err) {
+        res.status(400).json(err);
+    }
+});
+
+
+module.exports = router;
