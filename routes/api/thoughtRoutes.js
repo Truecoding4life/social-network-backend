4e389eb9db63bcfe401f9566b25e77df4a0e4fa6
Master <nghiemxthai@gmail.com>
2023-11-24 15:58:49 -0800

    update Thought routes is working

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,72 +1,75 @@
-const router = require('express').Router();
-const Thought = require('../../models/Thought');
-const User = require('../../models/User');
-
-// Create thought
-router.post('/', async (req, res) => {
-    try {
-        const thoughtCreation = await Thought.create(req.body);
-        const updateUserInfo = await User.findOneAndUpdate(
-            { "_id": req.body.userId },
-            { $addToSet: { thoughts: thoughtCreation._id } },
-            { new: true }
-          );
-        res.status(200).json(thoughtCreation);
-    } catch(err) {
-        res.status(500).json(err);
-    }
-});
+const router = require("express").Router();
+const Thought = require("../../models/Thought");
+const User = require("../../models/User");
 
 // Get all thought
-router.get('/', async (req, res) => {
-    try {
-        const thoughtData = await Thought.find({});
-        res.status(200).json(thoughtData);
-    } catch(err) {
-        res.status(500).json(err);
-    }
+router.get("/", async (req, res) => {
+  try {
+    const thoughtData = await Thought.find({});
+    res.status(200).json(thoughtData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// update a thought by id
-router.put('/:id', async (req, res) => {
-    try {
-        const thoughtUpdate = await Thought.findOneAndUpdate(
-            { _id: req.params.id },
-            { $set: {thoughText: req.body.thoughText} },
-            { runValidators: true, new: true }
-          );
-          if (!thoughtUpdate) {
-            return res.status(404).json({ message: 'No Thought with this id!' });
-          }
+// Create thought
+router.post("/", async (req, res) => {
+  try {
+    const thoughtCreation = await Thought.create(req.body);
+    const updateUserInfo = await User.findOneAndUpdate(
+      { _id: req.body.userId },
+      { $addToSet: { thoughts: thoughtCreation._id } },
+      { new: true }
+    );
+    res.status(200).json(thoughtCreation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update a thought by id
+router.put("/:id", async (req, res) => {
+  try {
+    const thoughtUpdate = await Thought.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    );
+    if (!thoughtUpdate) {
+      return res.status(404).json({ message: "No Thought with this id!" });
+    }
     
-          res.json(thoughtUpdate);
-        
-        res.status(200).json(thoughtUpdate);
-    } catch(err) {
-        res.status(500).json(err);
-    }
+    res.status(200).json(thoughtUpdate);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+// Force delete all thoughts
+router.delete("/delete", async (req, res) => {
+  const thoughtDeletion = await Thought.deleteMany({});
+  res.json(thoughtDeletion);
+});
 
 // delete a thought by id
-
-router.delete('/:id', async (req, res) => {
-    try {
-        const thoughtDeletion = await Thought.findOneAndDelete({ _id: req.params.id });
-        const userDeleteKey = await User.findOneAndUpdate(
-            { "_id": req.body.userId },
-            { $pull: { thoughts: req.params.id } },
-            { new: true}
-        );
-        if (!thoughtDeletion) {
-            return res.status(404).json({ message: 'No Thought with this id!' });
-          }
-    
-          res.json(thoughtDeletion);
-    } catch(err) {
-        res.status(500).json(err);
+router.delete("/:id", async (req, res) => {
+  try {
+    const thoughtDeletion = await Thought.findOneAndDelete({
+      _id: req.params.id,
+    });
+    const userDeleteKey = await User.findOneAndUpdate(
+      { _id: req.body.userId },
+      { $pull: { thoughts: req.params.id } },
+      { new: true }
+    );
+    if (!thoughtDeletion) {
+      return res.status(404).json({ message: "No Thought with this id!" });
     }
-});
 
+    res.json(thoughtDeletion);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
