35fd9922852dd9d6673df30fce345740a0db1a48
Master <nghiemxthai@gmail.com>
2023-11-24 20:29:22 -0800

    deleted controller

--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-const router = require ('express').Router
-const api = require('./api')
-
-router.use('/api', api);
-router.use((req, res) => res.send('Wrong route!'));
-
-module.exports = router;
--- models/Thought.js
+++ models/Thought.js
@@ -1,62 +1,36 @@
-const {Schema, model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-
-const reactionSchema = Schema( {
-    id:{
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId(),
-      },
-    body:{
-        type: String,
-        required: true
-    },
-    username: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-},
-{
+const reactionSchema = Schema(
+  {
+    id: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
+    body: { type: String, required: true },
+    username: { type: String },
+    createdAt: { type: Date, default: Date.now },
+  },
+  {
     toJSON: {
       getters: true,
     },
     id: false,
-})
-
-
-
-
+  }
+);
 
 const thoughtSchema = new Schema(
-    {   
-        thoughText: {
-            type: String,
-            required: true,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-        },
-        username: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "user",
-            },
-        ],
-        reaction: [reactionSchema],
+  {
+    thoughText: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    username: [{ type: Schema.Types.ObjectId, ref: "user",},],
+    reaction: [reactionSchema],},
+  {
+    toJSON: {
+      virtuals: true,
     },
-    {
-        toJSON: {
-            virtuals: true,
-        },
-        id: false,
-    }
+    id: false,
+  }
 );
 
 thoughtSchema.virtual("reactionCount").get(function () {
-    return this.reaction.length;
+  return this.reaction.length;
 });
 
 const Thought = model("thoughts", thoughtSchema);
